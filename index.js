cec4d652f7af316b3828d7336c02146546d33b52
Megan Chow <chow.megan@gmail.com>
2026-05-17 05:26:42 -0700

    setup themes

--- index.js
+++ index.js
@@ -1,13 +1,41 @@
-let MAX_NUMBER_POKEMON = 1350;
-let DIFFICULTIES = {
+const root = document.documentElement;
+
+const MAX_NUMBER_POKEMON = 1350;
+
+const DIFFICULTIES = {
   "easy": [2, 3],
   "medium": [3, 4],
   "hard": [4, 5]
 }
 
+const THEMES = {
+  forest: {
+    background: "url('images/forest.png')",
+    primary_colour: "#003c00",
+    secondary_colour: "green"
+  },
+  sea: {
+    background: "url('images/sea.png')",
+    primary_colour: "navy",
+    secondary_colour: "skyblue"
+  },
+  cave: {
+    background: "url('images/cave.png')",
+    primary_colour: "darkslategrey",
+    secondary_colour: "lightslategrey"
+  }
+}
+
 let difficulty = "medium";
 let numClicks = 0;
 let numMatches = 0;
+
+function applyTheme(newTheme) {
+  let theme = THEMES[newTheme];
+  root.style.setProperty('--background-image', theme.background);
+  root.style.setProperty('--primary-colour', theme.primary_colour);
+  root.style.setProperty('--secondary-colour', theme.secondary_colour);
+}
 
 async function selectRandomPokemon(numCards) {
   let urls = new Set();
@@ -52,7 +80,7 @@ async function dealCards() {
       row +=
         `
         <div id="card${index}" class="card">
-          <img id="img${index}" class="front_face" src="${pokemon[index].sprites.other['official-artwork'].front_default}" alt="">
+          <img id="img${index}" class="front_face" src="${pokemon[index].sprites.other['official-artwork'].front_default}" alt=""></img>
           <img class="back_face" src="back.webp" alt="">
         </div>
         `;
@@ -81,6 +109,7 @@ function win() {
 }
 
 async function setup () {
+  themeMenuSetup();
   await dealCards();
   let firstCard = undefined
   let secondCard = undefined
@@ -113,10 +142,32 @@ async function setup () {
       }
     }
   });
+
+
+  $("#theme_menu_button").on(("click"), function () {
+    $("#theme_menu").css("display", "flex");
+  });
+
+  $("#close_theme_menu").on(("click"), function () {
+    $("#theme_menu").css("display", "none");
+  });
 }
 
-function menu() {
+function themeMenuSetup() {
+  for (const [name, value] of Object.entries(THEMES)) {
+    $("#theme_options").append(
+      `
+      <div class="theme_option">
+        <button name="${name}" style="background-image: ${value.background}"></button>
+        <p>${String(name).charAt(0).toUpperCase() + String(name).slice(1)}</p>
+      </div>
+      `);
+  }
 
+  $("#theme_options").on("click", "button", function() {
+    const key = $(this).attr("name");
+    applyTheme(key);
+  });
 }
 
 $(document).ready(setup)
