60caa7bb938e7b92044572d3a43ed93473f64a67
Megan Chow <chow.megan@gmail.com>
2026-05-16 23:05:57 -0700

    prevent multiple flips if two cards are clicked. add function to fetch random pokemon.

--- index.js
+++ index.js
@@ -1,19 +1,25 @@
 let MAX_NUMBER_POKEMON = 1350;
 
-function selectRandomPokemon(numPokemon) {
-  let pokemonIds = Set();
-  while(pokemonIds.size < numPokemon) {
+async function selectRandomPokemon(numPokemon) {
+  let pokemonUrls = new Set();
+  while(pokemonUrls.size < numPokemon) {
     let random = Math.floor(Math.random() * MAX_NUMBER_POKEMON);
-    let response = await fetch(`https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=1`);
+    let response = await fetch(`https://pokeapi.co/api/v2/pokemon?offset=${random}&limit=1`);
     let jsonObj = await response.json();
+    
+    let pokemon = jsonObj.results[0];
+    pokemonUrls.add(pokemon.url);
   }
+  return pokemonUrls;
 }
 
 
 function setup () {
   let firstCard = undefined
   let secondCard = undefined
+  console.log(selectRandomPokemon(3));
   $(".card").on(("click"), function () {
+    if(firstCard && secondCard) return;
     $(this).toggleClass("flip");
 
     if (!firstCard)
@@ -25,19 +31,19 @@ function setup () {
         console.log("match")
         $(`#${firstCard.id}`).parent().off("click")
         $(`#${secondCard.id}`).parent().off("click")
+        firstCard = undefined;
+        secondCard = undefined;
       } else {
         console.log("no match")
-        let prevFirstCard = $(`#${firstCard.id}`).parent();
-        let prevSecondCard = $(`#${secondCard.id}`).parent();
         setTimeout(() => {
           // $(`#${firstCard.id}`).parent().toggleClass("flip")
           // $(`#${secondCard.id}`).parent().toggleClass("flip")
-          prevFirstCard.toggleClass("flip");
-          prevSecondCard.toggleClass("flip");
+          $(`#${firstCard.id}`).parent().toggleClass("flip");
+          $(`#${secondCard.id}`).parent().toggleClass("flip");
+          firstCard = undefined;
+          secondCard = undefined;
         }, 1000)
       }
-      firstCard = undefined;
-      secondCard = undefined;
     }
   });
 }
